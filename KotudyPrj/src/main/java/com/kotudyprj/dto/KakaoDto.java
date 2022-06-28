824d8b6327c7dc1c3c303cf9ea752b0c8d95ddcd
김시우 <tldn12gh@gmail.com>
2022-06-28 23:54:17 +0900

    init project - back end

---
+++ KotudyPrj/src/main/java/com/kotudyprj/dto/KakaoDto.java
@@ -0,0 +1,29 @@
+package com.kotudyprj.dto;
+
+public class KakaoDto {
+	private Object userId;
+	private Object nickName;
+	private Object image;
+	
+	public Object getUserId() {
+		return userId;
+	}
+	public void setUserId(Object userId) {
+		this.userId = userId;
+	}
+	public Object getNickName() {
+		return nickName;
+	}
+	public void setNickName(Object nickName) {
+		this.nickName = nickName;
+	}
+	public Object getImage() {
+		return image;
+	}
+	public void setImage(Object image) {
+		this.image = image;
+	}
+
+	
+
+}
