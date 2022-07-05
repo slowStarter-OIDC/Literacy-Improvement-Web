ee2da62f9d6e48867dfa8825d542114227c057d2
황선형 <ghkdtjsgud98@naver.com>
2022-07-05 23:00:24 +0900

    sunhyung commit

--- back-end/KotudyPrj/src/main/java/com/kotudyprj/MainController.java
+++ back-end/KotudyPrj/src/main/java/com/kotudyprj/MainController.java
@@ -1,7 +1,9 @@
 package com.kotudyprj;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,64 +20,100 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kotudyprj.dao.IKakaoDao;
 import com.kotudyprj.dao.IUserRankingDao;
 import com.kotudyprj.dto.KakaoDto;
+import com.kotudyprj.dto.WordItemDto;
+import com.kotudyprj.dto.WordsDto;
+import com.kotudyprj.service.DailyWordService;
 import com.kotudyprj.service.KakaoAPI;
+import com.kotudyprj.service.SearchWordService;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class MainController {
 
-	// DAO
-	@Autowired
-	IKakaoDao iKakaoDao;
+   // Service
+   @Autowired
+   DailyWordService dailyWordService;
 
-	@Autowired
-	KakaoAPI kakaoAPI;
-	
-	@Autowired
-	IUserRankingDao iUserRankingDao;
+   @Autowired
+   SearchWordService searchWordService;
 
-	HttpSession loginId;
+   // DAO
+   @Autowired
+   IKakaoDao iKakaoDao;
 
-	@RequestMapping("/")
-	public String root() throws Exception {
+   @Autowired
+   KakaoAPI kakaoAPI;
 
-		return "";
-	}
+   @Autowired
+   IUserRankingDao iUserRankingDao;
 
-	@GetMapping("/kakaoAuth")
-	public Object kakaoLogin(@RequestParam String code, HttpServletRequest req, KakaoDto kakaoDto) {
+   HttpSession loginId;
 
-		// ÌÅ¥ÎùºÏù¥Ïñ∏Ìä∏Ïùò Ïù¥Î©îÏùºÏù¥ Ï°¥Ïû¨Ìï† Îïå ÏÑ∏ÏÖòÏóê Ìï¥Îãπ Ïù¥Î©îÏùºÍ≥º ÌÜ†ÌÅ∞ Îì±Î°ù
-		HttpSession session = req.getSession(true);
-		String access_Token = kakaoAPI.getAccessToken(code);
-		HashMap<String, Object> userInfo = kakaoAPI.getUserInfo(access_Token);
-		// System.out.println("login Controller : " + userInfo);
+   @RequestMapping("/")
+   public String root() throws Exception {
 
-		if (userInfo.get("email") != null) {
+      return "";
+   }
 
-			kakaoDto.setUserId(userInfo.get("email"));
-			kakaoDto.setNickName(userInfo.get("nickname"));
-			kakaoDto.setImage(userInfo.get("profile_image"));
+   @GetMapping("/kakaoAuth")
+   public Object kakaoLogin(@RequestParam String code, HttpServletRequest req, KakaoDto kakaoDto) {
 
-			iKakaoDao.registerDao(kakaoDto.getUserId(), kakaoDto.getNickName(), kakaoDto.getImage());
-			// if (iUserRankingDao.checkRankingUserId(kakaoDto.getUserId()) == null) {
-			iUserRankingDao.createRankingInfo(kakaoDto.getUserId(), kakaoDto.getNickName(), kakaoDto.getImage());
-			// }
-			System.out.println(kakaoDto.getUserId() + " =========ÔøΩÎ∏òÔøΩÏî†ÔøΩÎµí");
-			List check = iKakaoDao.loginDao(kakaoDto.getUserId());
-			loginId = req.getSession();
-			loginId.setAttribute("userId", kakaoDto.getUserId());
+      // ≈¨∂Û¿Ãæ∆Æ¿« ¿Ã∏ﬁ¿œ¿Ã ¡∏¿Á«“ ∂ß ººº«ø° «ÿ¥Á ¿Ã∏ﬁ¿œ∞˙ ≈‰≈´ µÓ∑œ
+      HttpSession session = req.getSession(true);
+      String access_Token = kakaoAPI.getAccessToken(code);
+      HashMap<String, Object> userInfo = kakaoAPI.getUserInfo(access_Token);
+      // System.out.println("login Controller : " + userInfo);
 
-		}
-		System.out.println(loginId.getAttribute("userId"));
-		return loginId.getAttribute("userId");
-	}
+      if (userInfo.get("email") != null) {
 
-	@PostMapping("/kakaoLogout")
-	public String logout() {
+         kakaoDto.setUserId(userInfo.get("email"));
+         kakaoDto.setNickName(userInfo.get("nickname"));
+         kakaoDto.setImage(userInfo.get("profile_image"));
 
-		loginId.removeAttribute("userId");
-		return "index";
-	}
+         iKakaoDao.registerDao(kakaoDto.getUserId(), kakaoDto.getNickName(), kakaoDto.getImage());
+         // if (iUserRankingDao.checkRankingUserId(kakaoDto.getUserId()) == null) {
+         iUserRankingDao.createRankingInfo(kakaoDto.getUserId(), kakaoDto.getNickName(), kakaoDto.getImage());
+         // }
+         System.out.println(kakaoDto.getUserId() + " ========= ïÑ ù¥ îî");
+         List check = iKakaoDao.loginDao(kakaoDto.getUserId());
+         loginId = req.getSession();
+         loginId.setAttribute("userId", kakaoDto.getUserId());
+
+      }
+      System.out.println(loginId.getAttribute("userId"));
+      return loginId.getAttribute("userId");
+   }
+
+   @PostMapping("/kakaoLogout")
+   public String logout() {
+
+      loginId.removeAttribute("userId");
+      return "index";
+   }
+
+   @GetMapping("/dailyWords")
+   public List<WordsDto> dailyWords(WordsDto wordsDto) {
+      List<WordsDto> list = new ArrayList<>();
+      list = dailyWordService.dailyWords(wordsDto);
+      return list;
+
+   }
+
+   // πÆ¿Â ∞Àªˆ
+   @PostMapping("/searchWord")
+   public List<String> paraphraseCheck2(@RequestBody Map<String, String> body) {
+      List<String> list = new ArrayList<>();
+      list = searchWordService.paraphraseCheck(body);
+      return list;
+   }
+
+   // «—±πæÓ ±‚√ ªÁ¿¸ API»£√‚
+   @GetMapping("/oneWord")
+   public List<WordItemDto> oneWord(@RequestParam String q) {
+
+      List<WordItemDto> list = new ArrayList<>();
+      list = searchWordService.oneWord(q);
+      return list;
+   }
 
 }
